c116a2e9efe301ed1f3c3ecb89a74910407e5cb4
Om Bankar <bankarom0411@gmail.com>
2025-09-26 11:09:17 +0530

    Tree Node
    important for the trees to work

---
+++ tree/TreeNode.java
@@ -0,0 +1,78 @@
+package tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    // PreOrder Traversal: Root -> Left -> Right
+    public void preOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    // InOrder Traversal: Left -> Root -> Right
+    public void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.val + " ");
+        inOrder(node.right);
+    }
+
+    // PostOrder Traversal: Left -> Right -> Root
+    public void postOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.val + " ");
+    }
+ 
+    public static void main(String[] args) {
+        TreeNode obj = new TreeNode();
+
+        TreeNode one = new TreeNode(1);
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode four = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
+
+        one.left = two;
+        one.right = three;
+        two.right = four;
+        two.left = five;
+
+        TreeNode root = one;
+
+        System.out.println("Pre Order Traversal: ");
+        obj.preOrder(root);
+        System.out.println();
+
+        System.out.println("In Order Traversal: ");
+        obj.inOrder(root);
+        System.out.println();
+
+        System.out.println("Post Order Traversal: ");
+        obj.postOrder(root);
+        System.out.println();
+    }
+}
